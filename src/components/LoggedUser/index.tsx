22e784e10dfb34605d85e0390cc5dbd7ca999749
Jardel Rodrigues <rodriguinhosilvards96@gmail.com>
2024-12-01 15:23:52 -0300

    Ajuste para mostar usuario logado.

--- src/components/LoggedUser/index.tsx
+++ src/components/LoggedUser/index.tsx
@@ -2,10 +2,14 @@ import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import * as authService from '../../services/auth-service';
 import { ContextToken } from '../../utils/context-token';
+import { useEffect, useState } from 'react';
+import { UserDTO } from '../../models/user';
 
 export default function LoggedUser() {
 
     const { contextTokenPayload, setContextTokenPayload } = useContext(ContextToken);
+
+    const [user, setUser] = useState<UserDTO>();
 
     function handleLogoutClick() {
         authService.logout();
@@ -16,7 +20,7 @@ export default function LoggedUser() {
         contextTokenPayload && authService.isAuthenticated()
             ? (
                 <div className="dsc-logged-user">
-                    <p>{contextTokenPayload?.user_name}</p>
+                    <p>{user?.name}</p>
                     <span onClick={handleLogoutClick}>Sair</span>
                 </div>
             )
